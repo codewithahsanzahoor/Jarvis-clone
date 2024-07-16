01ac173d594769dbcbb356ca98107fbc9c39f046
Muhammad Ahsan Zahoor <109317164+codewithahsanzahoor@users.noreply.github.com>
2024-07-17 03:10:26 +0500

    adding news functionality by api request and speaking

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import requests
 
 # Initialize the recognizer
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
+
+# news api
+NEWS_API_KEY = "209981397982442497a8d87bbfa5363f"
 
 
 def speak(text):
@@ -33,7 +37,7 @@ def listen_for_wake_word():
             recognizer.adjust_for_ambient_noise(source)
             print("Listening for wake word...")
             audio_data = recognizer.listen(
-                source, timeout=2, phrase_time_limit=1)
+                source, timeout=10, phrase_time_limit=5)
             print("Recognizing wake word...")
             text = recognizer.recognize_google(audio_data).lower()
             print(f"Heard: {text}")
@@ -60,7 +64,7 @@ def listen_for_command():
         try:
             recognizer.adjust_for_ambient_noise(source)
             audio_data = recognizer.listen(
-                source, timeout=2, phrase_time_limit=1)
+                source, timeout=10, phrase_time_limit=5)
             print("Recognizing command...")
             command = recognizer.recognize_google(audio_data).lower()
             print(f"Command: {command}")
@@ -73,6 +77,23 @@ def listen_for_command():
             print(f"An error occurred: {e}")
 
     return ""
+
+
+def get_latest_news():
+    """
+    Fetches the latest news using the News API.
+    """
+    url = f"https://newsapi.org/v2/top-headlines?country=us&apiKey={
+        NEWS_API_KEY}"
+    response = requests.get(url)
+    news_data = response.json()
+
+    if news_data['status'] == 'ok':
+        articles = news_data['articles'][:5]  # Get the top 5 news articles
+        news_list = [article['title'] for article in articles]
+        return news_list
+    else:
+        return ["Failed to fetch news"]
 
 
 def perform_action(command):
@@ -91,7 +112,11 @@ def perform_action(command):
         song = command.lower().split(" ")[1]
         webbrowser.open(music[song])
         speak(f"playing {song}")
-    # Add more commands and actions as needed
+    elif "news" in command:
+        news_list = get_latest_news()
+        for news in news_list:
+            speak(news)
+            # print(news)
     else:
         print("Command not recognized.")
 
